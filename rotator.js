221395a5f34a27fbfbed26d071b9471e1d8deafb
cfcmeireles <carinacmeireles@gmail.com>
2022-10-27 19:33:19 +0100

    Recursive Content Rotator

--- rotator.js
+++ rotator.js
@@ -2,20 +2,19 @@
   "use strict";
 
   let counter = 1;
-  console.log(content.children);
 
   function contentRotator() {
-    $(`#content .slide:nth-child(${counter}) img`).fadeIn(2000, function () {
-      if ($(this).is("#content .slide:last-child img")) {
+    $(`#content .slide:nth-child(${counter})`).fadeIn(2000, function () {
+      if ($(this).is("#content .slide:last-child")) {
         setTimeout(function () {
-          $(`#content .slide:nth-child(${counter}) img`).fadeOut(2000, function () {
+          $(`#content .slide:nth-child(${counter})`).fadeOut(2000, function () {
             counter = 1;
             contentRotator();
           });
         }, 7000);
       } else {
         setTimeout(function () {
-          $(`#content .slide:nth-child(${counter}) img`).fadeOut(2000, function () {
+          $(`#content .slide:nth-child(${counter})`).fadeOut(2000, function () {
             counter++;
             contentRotator();
           });
@@ -24,27 +23,5 @@
     });
   }
 
-  /*  let timer = 1;
-
-  function paragraphRotator() {
-    $(`#paragraphs p:nth-child(${timer})`).fadeIn(2000, function () {
-      if ($(this).is("#paragraphs p:last-child")) {
-        setTimeout(function () {
-          $(`#paragraphs p:nth-child(${timer})`).fadeOut(2000, function () {
-            timer = 1;
-            paragraphRotator();
-          });
-        }, 7000);
-      } else {
-        setTimeout(function () {
-          $(`#paragraphs p:nth-child(${timer})`).fadeOut(2000, function () {
-            timer++;
-            paragraphRotator();
-          });
-        }, 7000);
-      }
-    });
-  } */
   contentRotator();
-  /*   paragraphRotator(); */
 })();
